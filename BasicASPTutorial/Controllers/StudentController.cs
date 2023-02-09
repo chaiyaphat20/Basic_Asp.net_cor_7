59992b8d688b31dd64d0e84516f23aead6630c56
chaiyaphat <chaiyaphat.b5922314@gmail.com>
2023-02-10 01:07:32 +0700

    [FEAT]:Add model and display value

--- BasicASPTutorial/Controllers/StudentController.cs
+++ BasicASPTutorial/Controllers/StudentController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BasicASPTutorial.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BasicASPTutorial.Controllers
 {
@@ -8,7 +9,28 @@ namespace BasicASPTutorial.Controllers
         //https://localhost:44336/Student/index
         public IActionResult Index()
         {
-            return View();
+            Student s1 = new Student();
+            s1.Id = 1;
+            s1.Name= "อาทรักสยาม";
+            s1.Score = 80;
+
+            var s2 = new Student();
+            s2.Id = 2;
+            s2.Name = "โจโจ้";
+            s2.Score = 45;
+
+            Student s3 = new();
+            s3.Id= 3;
+            s3.Name = "Jane";
+            s3.Score = 50;
+
+            List<Student> allStudent = new List<Student>();
+            allStudent.Add(s1);
+            allStudent.Add(s2);
+            allStudent.Add(s3);
+
+
+            return View(allStudent);
         }
 
         //https://localhost:44336/Student/Create
