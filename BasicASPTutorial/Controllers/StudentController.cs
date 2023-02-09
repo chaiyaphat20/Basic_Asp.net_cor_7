84045ec8f34bccb73fe794f3fed1811e4884771c
chaiyaphat <chaiyaphat.b5922314@gmail.com>
2023-02-10 00:09:39 +0700

    Add project files.

---
+++ BasicASPTutorial/Controllers/StudentController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BasicASPTutorial.Controllers
+{
+    //Map จะได้ route -> https://localhost:44336/Student
+    public class StudentController : Controller
+    {
+        //https://localhost:44336/Student/index
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //https://localhost:44336/Student/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //https://localhost:44336/Student/ShowScore/2   
+        //เมื่อตัวแปร params ที่รับ int id คือ 2 หรือ id
+        public IActionResult ShowScore(int id)
+        {
+            return Content($"คะแนนสอบของนักเรียน เลชที่ {id}");
+        }
+    }
+}
---
+++ BasicASPTutorial/Program.cs
@@ -0,0 +1,33 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews(); //Set ให้ Controller และ View ทำงาน
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection(); //กำหนดให้โปรเจครันแบบ https
+app.UseStaticFiles(); // ใช้ ตัว static file
+
+app.UseRouting(); //ใช้ route    ในโปรเจค
+
+app.UseAuthorization();//กำหนด สิทธิ์ในใช้งานโปรเจค
+
+app.MapControllerRoute(
+    name: "default", //ใช้สำหรับ กำหนด route default
+    pattern: "{controller=Student}/{action=Index}/{id?}");   //id? คือ optional
+    //Home คือ Controller แรกที่จะทำงาน
+    //Home คือการ เขียนย่อ เขียนแบบเต็มจะได้ HomeController
+
+    //action คือ กระบวนการทำงาน
+    //Index คือ method ที่ทำงานใน HomeController
+    //ใน HomeController ต้องมี action ชื่อ index ถึงจะทำงานได้
+
+app.Run();
